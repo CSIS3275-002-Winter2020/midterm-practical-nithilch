eb85adaa084570f45dc5b3100a7d2855ec47fa95
300288453 <300288453@R5107S25.ad.douglascollege.ca>
2020-02-14 13:35:06 -0800

    Commit and Push Comment

---
+++ Winter2020CSIS3275Midterm/src/HelloWorld.java
@@ -0,0 +1,8 @@
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Nithil Chigullapally");
+		System.out.println("chigullapallyn@student.douglascollege.ca");
+	}
+
+}
